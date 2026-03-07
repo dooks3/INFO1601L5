be941c2f0b3d9506a91debc9901660712721e5f4
dooks3 <jadooks06@gmail.com>
2026-03-07 16:54:41 -0400

    task 4.2 completed

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-console.log('1' == 1); 
-console.log(1 == true); 
-console.log("false" == false); 
-console.log("false" == true); 
+console.log('1' === 1); 
+console.log(1 === true); 
+console.log("true" === true);
 
-if("false"){
-  console.log("Hello false!");
+
+let grade = 67; 
+
+// Conditional Statements
+if(grade < 50){
+  console.log('Failed');
+}else{
+  console.log('Passed'); 
+}
+
+// while loop
+let num = 0; 
+while(num < 10){
+  console.log(num); 
+  num++;
+}
+
+// for loop
+for(let i = 0; i < 10; i++){
+  console.log(i);
 }
