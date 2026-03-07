50ec429340c5991fa0b132f115e65fa9737965c5
dooks3 <jadooks06@gmail.com>
2026-03-07 19:30:40 -0400

    lab 5 task 6 completed

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-let arr = [ -5, 16, 33, 42, 103, 344];
+function createPerson(name,weight,height){
+    this.name = name,
+    this.weight = weight,
+    this.height = height,
+    this.bmi = function (){
+      let bmi = this.weight / (this.height * this.height);
+      return bmi;
+    };
+}
 
-console.log(arr.includes(-5));//true
+let people = [
+    new createPerson("Sally",60,2.5), 
+    new createPerson("Ben",81,3),
+    new createPerson("Shelly",50,1.7)
+];
 
-arr.push(11);//adds item to the end
+function avgBMI(people){
+  let sum = 0; 
+  for (let person of people){
+    sum += person.bmi();
+  }
+  return sum/people.length; 
+}
 
-console.log(arr)//[ -5, 16, 33, 42, 103, 344, 11];
-
-let lastItem = arr.pop();//removes last item
-
-console.log(lastItem, arr);//11, [ -5, 16, 33, 42, 103, 344]
-
-arr.unshift(22);//adds item to the front
-
-console.log(arr);//[22, -5, 16, 33, 42, 103, 344]
-
-let firstItem = arr.shift();//removes first item
-console.log(firstItem, arr);//22, [-5, 16, 33, 42, 103, 344]
-
-let reversed = arr.reverse();//creates a new array in reverse order 
-console.log(reversed);//[344, 103, 42, 33, 16, -5]
-console.log(arr.join('-'));//"-5-103-16-33-344-42" joins array with provided separator
+console.log(avgBMI(people));
